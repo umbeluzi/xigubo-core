5475284c8e02d67c84fda2a6624cfbf944f1994d
Edson Michaque <edson@michaque.com>
2023-03-14 14:35:22 +0200

    Add close functions Publisher and Subscriber

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -3,13 +3,27 @@ package pubsub
 import "context"
 
 type Publisher interface {
-	Publish(ctx context.Context, topic string, data []byte) error
+	Publish(ctx context.Context, topic string, msg *Message) error
+	Close() error
 }
 
 type Subscriber interface {
 	Subscribe(ctx context.Context, topic string) (<-chan Message, error)
+	Close() error
 }
 
 type Message struct {
-	Data []byte
+	ID          string
+	ContentType string
+	Data        []byte
+	AckFunc     func() error
+	NackFunc    func() error
+}
+
+func (m Message) Ack() error {
+	return m.AckFunc()
+}
+
+func (m Message) Nack() error {
+	return m.NackFunc()
 }
